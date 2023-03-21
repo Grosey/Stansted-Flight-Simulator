b79c3ca5d8caaa9a8aeb646bce521384b2962bf0
Harrison Grose <captainhjg@hotmail.com>
2023-03-21 18:42:52 +0000

    Initial commit

---
+++ flight_sim_logbook.py
@@ -0,0 +1,156 @@
+import tkinter as tk
+from tkinter import ttk, simpledialog, messagebox
+import pickle
+from datetime import datetime, timedelta
+import firebase_admin
+from firebase_admin import credentials, initialize_app, db
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+cred = credentials.Certificate('stansted-flight-simulator-log-firebase-adminsdk-jxh61-4fb61497f6.json')  # Replace with the path to your JSON file
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://stansted-flight-simulator-log-default-rtdb.europe-west1.firebasedatabase.app/'  # Use your Realtime Database URL
+})
+
+class AddLogDialog(tk.Toplevel):
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent)
+        self.title("Add Log")
+        
+        self.log_data = {}
+
+        self.fields = [
+            ("date", "Select Date (DD-MM-YYYY):"),
+            ("captain", "Enter Captain Name:"),
+            ("journey_from", "Enter Departure Location:"),
+            ("journey_to", "Enter Arrival Location:"),
+            ("depart", "Enter Departure Time (HH:MM):"),
+            ("arrival", "Enter Arrival Time (HH:MM):"),
+            ("total_time", "Enter Total Time (HH:MM):"),
+            ("remarks", "Enter Remarks:"),
+        ]
+
+        self.entries = {}
+
+        for field, label_text in self.fields:
+            label = tk.Label(self, text=label_text)
+            label.pack(pady=5)
+            entry = tk.Entry(self)
+            entry.pack(pady=5)
+            self.entries[field] = entry
+
+        submit_button = tk.Button(self, text="Submit", command=self.submit)
+        submit_button.pack(pady=5)
+
+    def submit(self):
+        for field, entry in self.entries.items():
+            self.log_data[field] = entry.get()
+        self.destroy()
+
+class LogBook:
+    def __init__(self, master):
+        
+        # Get a reference to the logs in the Firebase Realtime Database
+        self.logs_ref = db.reference('logs')
+        
+        self.master = master
+        self.master.title("Stansted Flight Simulator Log Book")
+        self.master.iconbitmap('thumbnail_cf23fcb6.ico')
+        self.logs = []
+        self.load_logs()
+
+        self.log_listbox = tk.Listbox(self.master, width=150, height=20)
+        self.log_listbox.pack(padx=10, pady=10)
+
+        self.update_listbox()
+
+        self.add_button = tk.Button(self.master, text="Add Log", command=self.add_log)
+        self.add_button.pack(side=tk.LEFT, padx=5)
+
+        self.delete_button = tk.Button(self.master, text="Delete Log", command=self.delete_log)
+        self.delete_button.pack(side=tk.LEFT, padx=5)
+
+        # Add the exit button
+        self.exit_button = tk.Button(self.master, text="Exit", command=self.master.destroy)
+        self.exit_button.pack(side=tk.RIGHT, padx=5, pady=10)
+
+        
+
+    def load_logs(self):
+        logs_ref = db.reference('/logs')
+        logs_data = logs_ref.get()
+        if logs_data:
+            self.logs = []
+            for log in logs_data:
+                log["date"] = datetime.strptime(log["date"], "%d-%m-%Y")
+                self.logs.append(log)
+        else:
+            self.logs = []
+
+
+
+    def save_logs(self):
+        logs_to_save = []
+        for log in self.logs:
+            new_log = log.copy()
+            new_log["date"] = log["date"].strftime("%d-%m-%Y")
+            logs_to_save.append(new_log)
+            
+        self.logs_ref.set({i: log for i, log in enumerate(logs_to_save)})
+
+
+
+    def update_listbox(self):
+        self.log_listbox.delete(0, tk.END)
+        for log in self.logs:
+            formatted_date = log["date"].strftime("%d-%m-%Y")
+            log_text = f"Date: {formatted_date} | Captain: {log['captain']} | Journey: {log['journey_from']} to {log['journey_to']} | Depart: {log['depart']} | Arrival: {log['arrival']} | Total Time: {log['total_time']} | Remarks: {log['remarks']}"
+            self.log_listbox.insert(tk.END, log_text)
+
+
+
+
+
+    def add_log(self):
+        add_log_dialog = AddLogDialog(self.master)
+        self.master.wait_window(add_log_dialog)
+
+        log_data = add_log_dialog.log_data
+        if all(value for key, value in log_data.items() if key != "remarks"):
+            log_data["date"] = datetime.strptime(log_data["date"], "%d-%m-%Y")
+            self.logs.append(log_data)
+            self.update_listbox()
+            self.save_logs()
+
+    def edit_log(self):
+        selected_index = self.log_listbox.curselection()
+        if not selected_index:
+            messagebox.showwarning("Warning", "Please select a log to edit.")
+            return
+
+        old_log = self.logs[selected_index[0]]
+        for key, value in old_log.items():
+            new_value = tk.simpledialog.askstring("Edit Log", f"Edit {key}:", initialvalue=value)
+            if new_value:
+                old_log[key] = new_value
+            else:
+                return
+
+        self.update_listbox()
+        self.save_logs()
+
+    def delete_log(self):
+        selected_index = self.log_listbox.curselection()
+        if not selected_index:
+            messagebox.showwarning("Warning", "Please select a log to delete.")
+            return
+
+        self.logs.pop(selected_index[0])
+        self.update_listbox()
+        self.save_logs()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    LogBook(root)
+    root.mainloop()
